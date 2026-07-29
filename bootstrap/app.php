5631c66b2ea463a43c2e24e8f7bcffb339092599
rdxrio123 <riodwibinafitriono@gmail.com>
2026-07-29 18:20:53 +0700

    debug: echo exceptions in app.php to debug Vercel build

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,5 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->reportable(function (\Throwable $e) {
+            echo "FATAL ERROR CAUGHT: " . $e->getMessage() . "\n";
+            echo $e->getTraceAsString() . "\n";
+        });
     })->create();
