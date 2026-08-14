922cf513ff760010ceff7ac4d9bcc64db2827dc4
rdxrio123 <riodwibinafitriono@gmail.com>
2026-08-14 23:12:42 +0700

    Clean up garbled files and typo

--- src/components/BottomNav.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { NavTab } from '../types';
-import { P, T, IND } from '../constants';
-
-export function BottomNav({ tab, onTab }
-
-export function NavIcon({ id, c, active }
-
--- src/screens/LoginScreen.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from 'react';
-import { Screen, HistTab } from '../types';
-import { P, V, T, AM, RD, IND } from '../constants';
-import { FU, SIR } from '../components/Animations';
-import { InputField } from '../components/InputField';
-
-export function LoginScreen({ onLogin }
