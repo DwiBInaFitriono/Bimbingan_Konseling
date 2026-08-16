867c5d7c6caba5f1bf56ad9f563fff74696a228c
rdxrio123 <riodwibinafitriono@gmail.com>
2026-08-16 20:46:21 +0700

    fix(db): update TiDB cloud credentials in serverless API

--- android-app/api/v1/db.ts
+++ android-app/api/v1/db.ts
@@ -2,6 +2,6 @@ import { connect } from '@tidbcloud/serverless';
 
 // Default connection string from your BK project environment.
 // In Vercel, it is recommended to put this in Environment Variables (DATABASE_URL)
-const connectionString = process.env.DATABASE_URL || 'mysql://29hkS1jfx2jKirT.root:RTpfTnYqsilcW7aS@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/test?ssl={"rejectUnauthorized":true}';
+const connectionString = process.env.DATABASE_URL || 'mysql://29hkS1jfx2jKirT.root:Uv24c1b0gb2XrzuQ@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/test?ssl={"rejectUnauthorized":true}';
 
 export const db = connect({ url: connectionString });
